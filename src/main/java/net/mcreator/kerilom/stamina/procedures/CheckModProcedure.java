143ebfc489a5986776786e4ffb5055d7a2e81b17
Kerilom (Glebun08) <89039577+Gleb-Lisovskiy@users.noreply.github.com>
2025-09-26 07:40:29 +0400

    fix crash

---
+++ src/main/java/net/mcreator/kerilom/stamina/procedures/CheckModProcedure.java
@@ -0,0 +1,33 @@
+package net.mcreator.kerilom.stamina.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.entity.Entity;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class CheckModProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player);
+		}
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (ModList.get().isLoaded("combatroll")) {
+			TickPlayerRollingProcedure.execute(entity);
+		}
+	}
+}
