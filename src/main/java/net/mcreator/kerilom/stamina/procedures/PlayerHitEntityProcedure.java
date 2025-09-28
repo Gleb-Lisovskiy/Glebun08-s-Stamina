d8039f37931f4e3dd748f399b70464df9ae3a1ab
Kerilom (Glebun08) <89039577+Gleb-Lisovskiy@users.noreply.github.com>
2025-09-29 00:39:29 +0400

    Fix
    Fixed when a player hits an entity with a ranged weapon and stamina is spent because of this.
    Added compatibility with the Vic's Point Blank mod.

--- src/main/java/net/mcreator/kerilom/stamina/procedures/PlayerHitEntityProcedure.java
+++ src/main/java/net/mcreator/kerilom/stamina/procedures/PlayerHitEntityProcedure.java
@@ -15,6 +15,8 @@ import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.client.Minecraft;
@@ -29,24 +31,24 @@ public class PlayerHitEntityProcedure {
 	@SubscribeEvent
 	public static void onEntityAttacked(LivingDamageEvent.Post event) {
 		if (event.getEntity() != null) {
-			execute(event, event.getEntity(), event.getSource().getEntity());
+			execute(event, event.getSource(), event.getEntity(), event.getSource().getEntity());
 		}
 	}
 
-	public static void execute(Entity entity, Entity sourceentity) {
-		execute(null, entity, sourceentity);
+	public static void execute(DamageSource damagesource, Entity entity, Entity sourceentity) {
+		execute(null, damagesource, entity, sourceentity);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity, Entity sourceentity) {
-		if (entity == null || sourceentity == null)
+	private static void execute(@Nullable Event event, DamageSource damagesource, Entity entity, Entity sourceentity) {
+		if (damagesource == null || entity == null || sourceentity == null)
 			return;
-		if (ConfigConfiguration.HITTED_ENTITY_BOOLEAN.get()) {
-			if (!entity.isInvulnerable() && sourceentity instanceof Player && (getEntityGameType(sourceentity) == GameType.SURVIVAL || getEntityGameType(sourceentity) == GameType.ADVENTURE)) {
-				if (((sourceentity instanceof LivingEntity _entUseItem5 ? _entUseItem5.getUseItem() : ItemStack.EMPTY).getItem() instanceof AxeItem
-						|| (sourceentity instanceof LivingEntity _entUseItem7 ? _entUseItem7.getUseItem() : ItemStack.EMPTY).getItem() instanceof PickaxeItem
-						|| (sourceentity instanceof LivingEntity _entUseItem9 ? _entUseItem9.getUseItem() : ItemStack.EMPTY).getItem() instanceof ShovelItem
-						|| (sourceentity instanceof LivingEntity _entUseItem11 ? _entUseItem11.getUseItem() : ItemStack.EMPTY).getItem() instanceof SwordItem
-						|| (sourceentity instanceof LivingEntity _entUseItem13 ? _entUseItem13.getUseItem() : ItemStack.EMPTY).getItem() instanceof HoeItem)
+		if (ConfigConfiguration.HITTED_ENTITY_BOOLEAN.get() && !CheckWeaponPointBlankProcedure.execute(sourceentity)) {
+			if (!entity.isInvulnerable() && sourceentity instanceof Player && (getEntityGameType(sourceentity) == GameType.SURVIVAL || getEntityGameType(sourceentity) == GameType.ADVENTURE) && damagesource.is(DamageTypes.PLAYER_ATTACK)) {
+				if (((sourceentity instanceof LivingEntity _entUseItem6 ? _entUseItem6.getUseItem() : ItemStack.EMPTY).getItem() instanceof AxeItem
+						|| (sourceentity instanceof LivingEntity _entUseItem8 ? _entUseItem8.getUseItem() : ItemStack.EMPTY).getItem() instanceof PickaxeItem
+						|| (sourceentity instanceof LivingEntity _entUseItem10 ? _entUseItem10.getUseItem() : ItemStack.EMPTY).getItem() instanceof ShovelItem
+						|| (sourceentity instanceof LivingEntity _entUseItem12 ? _entUseItem12.getUseItem() : ItemStack.EMPTY).getItem() instanceof SwordItem
+						|| (sourceentity instanceof LivingEntity _entUseItem14 ? _entUseItem14.getUseItem() : ItemStack.EMPTY).getItem() instanceof HoeItem)
 						&& sourceentity.getData(KstaminaModVariables.PLAYER_VARIABLES).stamina_regen_cd < (double) ConfigConfiguration.STAMINA_REGEN_CD_HITTED_ENTITY.get() / 1.65) {
 					{
 						KstaminaModVariables.PlayerVariables _vars = sourceentity.getData(KstaminaModVariables.PLAYER_VARIABLES);
